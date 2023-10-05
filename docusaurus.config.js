247683de09b53fd499d7ea2d710011e55d2fd6f5
mineplexio <info@mineplex.io>
2023-10-05 16:36:24 +0300

    Update projectName in Docusaurus config
    Project name in Docusaurus configuration has been updated from 'docusuaurus' to 'docs'. This change aligns the project name with its actual content, improving relevance and clarity.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -19,7 +19,7 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'crossfichain', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  projectName: 'docs', // Usually your repo name.
 
   onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
